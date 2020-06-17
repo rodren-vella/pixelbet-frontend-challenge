a48091d2fffa758aab5fc996f2ed31d1bdbca6e9
rodren-vella <45037990+rodren-vella@users.noreply.github.com>
2020-06-17 15:10:33 +0200

    favicon and comments

--- src/components/diceSelection.js
+++ src/components/diceSelection.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const diceSelection = ({ dices, selectDice }) => {
+const diceSelection = ({ allDice, selectDice }) => {
   return (
     <div className="betPanel__diceSlection">
       <span className="betPanel__instructions">Tap to change selection</span>
-      <ul className="betPanel__dices" id="js-dices">
-        {dices.map((dice, i) => (
+      <ul className="betPanel__alldice" id="js-alldice">
+        {allDice.map((dice, i) => (
           <li key={i} onClick={() => selectDice(dice.diceNumber, i)}>
             {dice.diceImage}
           </li>
