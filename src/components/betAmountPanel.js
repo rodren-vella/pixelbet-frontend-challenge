a6812ba48922adefbb99b6ed278c1b3fe678ce66
rodren-vella <45037990+rodren-vella@users.noreply.github.com>
2020-06-19 10:52:58 +0200

    refactored BEM and Comments

--- src/components/betAmountPanel.js
+++ src/components/betAmountPanel.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { ReactComponent as Minus } from "../assets/minus.svg";
-import { ReactComponent as Plus } from "../assets/plus.svg";
+import React from 'react'
+import { ReactComponent as Minus } from '../assets/minus.svg'
+import { ReactComponent as Plus } from '../assets/plus.svg'
 
 const betAmountPanel = ({ betAmount, decreaceBet, increaceBet }) => {
   return (
-    <div className="betPanel__amountPanel">
-      <Minus width="44" onClick={decreaceBet} />
-      <span>{betAmount}</span>
-      <Plus width="44" onClick={increaceBet} />
+    <div className="bet-panel__bet-amount">
+      <Minus
+        width="44"
+        onClick={decreaceBet}
+        className="bet-panel__bet-control--less"
+      />
+      <span className="bet-panel__bet-value">{betAmount}</span>
+      <Plus
+        width="44"
+        onClick={increaceBet}
+        className="bet-panel__bet-control--more"
+      />
     </div>
-  );
-};
+  )
+}
 
-export default betAmountPanel;
+export default betAmountPanel
